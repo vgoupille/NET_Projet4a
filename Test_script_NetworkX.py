42e1613839406edd20e9a4ad1d8758f32bd6de51
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-10-24 23:03:24 +0200

    Add test script for Networkx

---
+++ Test_script_NetworkX.py
@@ -0,0 +1,4 @@
+# Description: Test script for Networkx
+# https://networkx.org/documentation/stable/reference/readwrite/generated/networkx.readwrite.graphml.read_graphml.html
+# read_graphml(path, node_type=<class 'str'>, edge_key_type=<class 'int'>, force_multigraph=False)
+read_graphml("Data/reactome-77-reaction_R-HSA-5218826.graphml")
