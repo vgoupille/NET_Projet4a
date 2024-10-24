90ef7366797ce18272fcd05a480d7e85bc741552
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-10-24 22:55:56 +0200

    Update create_envi.bash script to include a comment explaining its purpose

--- create_envi.bash
+++ create_envi.bash
@@ -1,4 +1,4 @@
-conda env create --file environment.yml
+#Just a simple script to create a conda environment from a yml file
 conda activate myenvTEST
 conda env list
 #conda env update --file environment.yml --prune #If you modify the environment.yml file and want to update an existing environment to reflect the changes, you can use the following command
