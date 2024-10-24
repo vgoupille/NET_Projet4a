ee08bd9a902599ad6e2f878b0b691daa83feb880
Valentin Goupille <125963826+vgoupille@users.noreply.github.com>
2024-10-24 22:54:01 +0200

    Update environment.yml with modified environment name

---
+++ create_envi.bash
@@ -0,0 +1,4 @@
+conda env create --file environment.yml
+conda activate myenvTEST
+conda env list
+#conda env update --file environment.yml --prune #If you modify the environment.yml file and want to update an existing environment to reflect the changes, you can use the following command
